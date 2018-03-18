3e1f91fb8209ee4527bdc0342e3ee8168e68f5c2
sayantansatpati <sayantan.satpati.sfbay@gmail.com>
2018-03-17 18:27:16 -0700

    Updating notebook with all changes made in local

--- my_answers.py
+++ my_answers.py
@@ -13,6 +13,11 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    for i in range(0, len(series) - window_size):
+        j = i + window_size
+        X.append(series[i:j])
+        y.append(series[j])
+
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,12 +28,29 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
+    from string import ascii_lowercase
     punctuation = ['!', ',', '.', ':', ';', '?']
+
+    # cleaned_text = []
+    # for w in text:
+    #     if w in punctuation or w in ascii_lowercase:
+    #         cleaned_text.append(c)
+    #
+    # text = ''.join(cleaned_text)
+    chars_unique = set(text)
+    print(chars_unique)
+    for c in chars_unique:
+        if c not in list(ascii_lowercase) and c not in punctuation:
+            text.replace(c, ' ')
 
     return text
 
@@ -38,9 +60,19 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for i in range(0, len(text) - window_size, step_size):
+        j = i + window_size
+        inputs.append(text[i:j])
+        outputs.append(text[j])
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(keras.layers.Activation('softmax'))
+
+    return model
